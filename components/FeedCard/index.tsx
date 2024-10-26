ac6e317d334ecaec535937d673385fead03f9504
Naveed-23 <naveedbh23@gmail.com>
2024-10-26 13:56:21 +0530

    check

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -4,6 +4,7 @@ import { AiOutlineHeart } from "react-icons/ai"
 import { BiMessageRounded, BiUpload } from "react-icons/bi"
 import { FaRetweet } from "react-icons/fa"
 
+
 const FeedCard: React.FC = () => {
     return <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
         <div className="grid grid-cols-12 gap-3">
